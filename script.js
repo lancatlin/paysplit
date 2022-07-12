d7ecfe3cddebe0c485bf4f305dc7e168850dc31b
wancat <wancat@wancat.cc>
2022-07-12 23:20:37 +0800

    add button

--- script.js
+++ script.js
@@ -5,16 +5,13 @@ function onEnter(callback) {
     return (ev) => {
         if (ev.key === "Enter") {
             callback();
-            settlement()
         }
     }
 }
 
 function addPerson() {
     elem = document.getElementById("name")
-    if (elem.value in people) {
-        return
-    }
+    if (elem.value in people) return
     people[elem.value] = 0
     console.log(elem.value)
     li = document.createElement("li")
@@ -26,6 +23,7 @@ function addPerson() {
     option.innerText = elem.value
     document.getElementById("payer-name").appendChild(option)
     elem.value = ""
+    settlement()
 }
 
 function addRecord() {
@@ -42,6 +40,7 @@ function addRecord() {
 
     amountElem.value = ""
     console.log(people)
+    settlement()
 }
 
 function removeAllChildNodes(parent) {
